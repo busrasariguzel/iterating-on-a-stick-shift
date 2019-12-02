bda4858a4d34357736501aa008ac0c8f6bb22e7e
busrasariguzel <busra.sariguzel@codeimmersives.com>
2019-12-02 13:57:31 -0500

    complete

--- main.js
+++ main.js
@@ -1,14 +1,53 @@
-const makeDino = function() {
+const makeDino = function(species, period, carnivore, extinct=false) {
+  return { 
+    species: species,
+    period: period,
+    carnivore: carnivore,
+    extinct: extinct,
+
+}
 }
 
-const makeSingular = function() {
+const makeSingular = function(dino) {
+  if (!dino['species'].endsWith('us')) {
+    return dino
+  }
+  else {
+    return { 
+    species: dino['species'].slice(0,-2),
+    period: dino['period'],
+    carnivore: dino['carnivore'],
+    extinct: dino['extinct'],
+}
+}
+}
+const truncateSpecies = function(dino) {
+  if (dino['species'].length < 11) {
+    return dino
+  }
+  else {
+  
+      const truncatedDino = {
+      species: dino['species'].slice(0,7) + '...',
+      period: dino['period'],
+      carnivore: dino['carnivore'],
+      extinct: dino['extinct'],
+      
+  }
+  return truncatedDino
+  }
+
 }
 
-const truncateSpecies = function() {
-}
-
-const makeExtinct = function() {
-
+const makeExtinct = function(dino) {
+  
+  const extinctDino = {
+    species: dino['species'],
+    period: dino['period'],
+    carnivore: dino['carnivore'],
+    extinct: true,
+  }
+  return extinctDino
 }
 
 module.exports = {
